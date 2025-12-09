58f3387f08c15f57bcb89afa22dc8373c740032a
yurvon-screamo <yurvon@ya.ru>
2025-12-09 23:03:07 +0300

    ui kit basic

--- src/domain/dictionary/mod.rs
+++ src/domain/dictionary/mod.rs
@@ -1,7 +1,9 @@
+mod jlpt;
 mod kanji;
 mod radical;
 mod vocabulary;
 
+pub use jlpt::JLPT_DB;
 pub use kanji::{KANJI_DB, KanjiInfo};
 pub use radical::{RADICAL_DB, RadicalInfo};
 pub use vocabulary::{VOCABULARY_DB, VocabularyInfo};
