db98d63ff59ce9238a7aeee93aedd8bb52256825
yurvon-screamo <yurvon@ya.ru>
2026-02-14 09:03:09 +0300

    workable Login

--- origa_ui/src/app.rs
+++ origa_ui/src/app.rs
@@ -1,9 +1,11 @@
-use crate::routes::Routes;
+use crate::repository::InMemoryUserRepository;
+use crate::routes::AppRoutes;
 use leptos::prelude::*;
 
 #[component]
 pub fn App() -> impl IntoView {
+    provide_context(InMemoryUserRepository::new());
     view! {
-        <Routes />
+        <AppRoutes />
     }
 }
