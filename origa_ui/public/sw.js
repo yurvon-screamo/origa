3fd89245072247e859c0d069409aa12a053140a7
yurvon-screamo <yurvon@ya.ru>
2026-02-26 17:24:29 +0300

    add pwa

---
+++ origa_ui/public/sw.js
@@ -0,0 +1,57 @@
+const CACHE_NAME = 'origa-v1';
+const STATIC_ASSETS = [
+  '/',
+  '/index.html',
+  '/pkg/origa_ui.js',
+  '/pkg/origa_ui_bg.wasm',
+  '/public/logo.png',
+  '/public/logo-192.png',
+  '/public/logo-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(STATIC_ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      
+      return fetch(event.request).then((response) => {
+        if (!response || response.status !== 200 || response.type !== 'basic') {
+          return response;
+        }
+        
+        const responseToCache = response.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseToCache);
+        });
+        
+        return response;
+      });
+    })
+  );
+});
