51a0407383fd2469f76996de7e2c7be9f6c403c4
yurvon-screamo <yurvon@ya.ru>
2026-02-28 19:10:00 +0300

    add tts

--- origa_ui/src/ui_components/audio_buttons.rs
+++ origa_ui/src/ui_components/audio_buttons.rs
@@ -8,15 +8,12 @@ pub fn AudioButtons(
     #[prop(into)] text: String,
     #[prop(optional, into)] class: Signal<String>,
 ) -> impl IntoView {
-    let text_normal = text.clone();
-    let text_slow = text.clone();
-
     view! {
         <div class=move || format!("flex gap-2 {}", class.get())>
             <button
                 class="btn btn-ghost px-2 py-1"
                 on:click=move |_| {
-                    let reading = get_reading_from_text(&text_normal);
+                    let reading = get_reading_from_text(&text);
                     if is_speech_supported() {
                         let _ = speak_text(&reading, 1.0);
                     }
@@ -24,18 +21,6 @@ pub fn AudioButtons(
                 disabled=move || !is_speech_supported()
             >
                 <Icon icon=icondata::LuVolume2 width="1.25em" height="1.25em" />
-            </button>
-            <button
-                class="btn btn-ghost px-2 py-1"
-                on:click=move |_| {
-                    let reading = get_reading_from_text(&text_slow);
-                    if is_speech_supported() {
-                        let _ = speak_text(&reading, 0.5);
-                    }
-                }
-                disabled=move || !is_speech_supported()
-            >
-                <Icon icon=icondata::LuClock width="1.25em" height="1.25em" />
             </button>
         </div>
     }
