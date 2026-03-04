332cd0ba758e3f0067184d85c5796bfd1a09ae9c
yurvon-screamo <yurvon@ya.ru>
2026-03-04 09:49:14 +0300

    fea

--- origa_ui/src/pages/login/login_form.rs
+++ origa_ui/src/pages/login/login_form.rs
@@ -41,9 +41,6 @@ pub fn LoginForm(
                 <div class="absolute inset-0 flex items-center">
                     <div class="w-full border-t border-[var(--border-color)]"></div>
                 </div>
-                <div class="relative flex justify-center text-sm">
-                    <span class="px-2 bg-[var(--bg-primary)] text-[var(--fg-muted)]">"или"</span>
-                </div>
             </div>
 
             <OAuthButtons />
