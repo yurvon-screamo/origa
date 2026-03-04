2aef30305e6f6543edf7fd1266f5a54995b41fdc
yurvon-screamo <yurvon@ya.ru>
2026-03-04 09:45:12 +0300

    fea

--- origa_ui/src/ui_components/audio_buttons.rs
+++ origa_ui/src/ui_components/audio_buttons.rs
@@ -8,20 +8,27 @@ pub fn AudioButtons(
     #[prop(into)] text: String,
     #[prop(optional, into)] class: Signal<String>,
 ) -> impl IntoView {
+    let reading = get_reading_from_text(&text);
+    let has_reading = !reading.is_empty();
+
     view! {
-        <div class=move || format!("flex gap-2 {}", class.get())>
-            <button
-                class="btn btn-ghost px-2 py-1"
-                on:click=move |_| {
-                    let reading = get_reading_from_text(&text);
-                    if is_speech_supported() {
-                        let _ = speak_text(&reading, 1.0);
+        <Show when=move || has_reading>
+            <div class=move || format!("flex gap-2 {}", class.get())>
+                <button
+                    class="btn btn-ghost px-2 py-1"
+                    on:click={
+                        let reading = reading.clone();
+                        move |_| {
+                            if is_speech_supported() {
+                                let _ = speak_text(&reading, 1.0);
+                            }
+                        }
                     }
-                }
-                disabled=move || !is_speech_supported()
-            >
-                <Icon icon=icondata::LuVolume2 width="1.25em" height="1.25em" />
-            </button>
-        </div>
+                    disabled=move || !is_speech_supported()
+                >
+                    <Icon icon=icondata::LuVolume2 width="1.25em" height="1.25em" />
+                </button>
+            </div>
+        </Show>
     }
 }
