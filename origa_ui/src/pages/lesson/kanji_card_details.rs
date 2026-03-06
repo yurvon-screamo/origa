f6ec15484f0ac166ba9f146a427e0ec5748959de
yurvon-screamo <yurvon@ya.ru>
2026-03-06 19:03:35 +0300

    KanjiViewMode::Frames

--- origa_ui/src/pages/lesson/kanji_card_details.rs
+++ origa_ui/src/pages/lesson/kanji_card_details.rs
@@ -36,15 +36,20 @@ pub fn KanjiCardDetails(
             <div class="my-6 space-y-4 max-w-max mx-auto">
                 <ReadingGroup label="音読み" readings=on_readings_stored />
                 <ReadingGroup label="訓読み" readings=kun_readings_stored />
-            </div>
 
-            <Show when=move || radicals_stored.get_value().is_some()>
-                <div class="my-6">
-                    <Text size=TextSize::Default variant=TypographyVariant::Muted>
-                        {format!("Радикалы: {}", radicals_stored.get_value().unwrap_or_default())}
-                    </Text>
-                </div>
-            </Show>
+                <Show when=move || radicals_stored.get_value().is_some()>
+                    <div class="flex gap-4 items-start text-left">
+                        <div class="w-16 shrink-0">
+                            <Text size=TextSize::Default variant=TypographyVariant::Muted>
+                                "Радикалы"
+                            </Text>
+                        </div>
+                        <Text size=TextSize::Default>
+                            {radicals_stored.get_value().unwrap_or_default()}
+                        </Text>
+                    </div>
+                </Show>
+            </div>
 
             <Show when=move || examples_stored.get_value().is_some()>
                 <div class="my-6">
