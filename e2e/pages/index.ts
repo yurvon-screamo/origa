2c3fcf2019407d5d1df7e6f770b82be3ed9f86cd
yurvon-screamo <yurvon@ya.ru>
2026-03-08 09:29:49 +0300

    add ocr protoc

--- e2e/pages/index.ts
+++
@@ -1,8 +0,0 @@
-export { GrammarPage } from "./GrammarPage";
-export { HomePage } from "./HomePage";
-export { KanjiPage } from "./KanjiPage";
-export { LessonPage } from "./LessonPage";
-export { LoginPage } from "./LoginPage";
-export { ProfilePage } from "./ProfilePage";
-export { SetsPage } from "./SetsPage";
-export { WordsPage } from "./WordsPage";
