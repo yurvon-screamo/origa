6af718aa85d190fa8f0a0079596aab4d7f00a64a
yurvon-screamo <yurvon@ya.ru>
2026-03-08 12:18:35 +0300

    add test draft

--- origa/src/use_cases/tests/fixtures/mod.rs
+++ origa/src/use_cases/tests/fixtures/mod.rs
@@ -4,4 +4,7 @@ mod test_data;
 
 pub use in_memory_repository::InMemoryUserRepository;
 pub use in_memory_well_known_set_loader::InMemoryWellKnownSetLoader;
-pub use test_data::{create_test_kanji_card, create_test_vocab_card, create_user_with_vocab_cards, init_test_dictionaries};
+pub use test_data::{
+    create_test_kanji_card, create_test_vocab_card, create_user_with_vocab_cards,
+    init_test_dictionaries,
+};
