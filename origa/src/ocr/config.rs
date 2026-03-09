cc0b7f9c1a7ef0b31ff3e92a2aa3aaa04d757fc6
yurvon-screamo <yurvon@ya.ru>
2026-03-09 17:05:34 +0300

    add layout: fmt

--- origa/src/ocr/config.rs
+++ origa/src/ocr/config.rs
@@ -3,10 +3,6 @@ pub struct ModelConfig {
     pub ocr_base_url: String,
     pub ocr_model_name: String,
     pub ocr_cache_dir: String,
-
-    pub layout_base_url: String,
-    pub layout_model_name: String,
-    pub layout_filename: String,
 }
 
 impl ModelConfig {
@@ -15,9 +11,6 @@ impl ModelConfig {
             ocr_base_url: "https://huggingface.co".to_string(),
             ocr_model_name: "l0wgear/manga-ocr-2025-onnx".to_string(),
             ocr_cache_dir: "manga-ocr-model-".to_string(),
-            layout_base_url: "https://huggingface.co".to_string(),
-            layout_model_name: "wybxc/DocLayout-YOLO-DocStructBench-onnx".to_string(),
-            layout_filename: "doclayout_yolo_docstructbench_imgsz1024.onnx".to_string(),
         }
     }
 
@@ -25,17 +18,11 @@ impl ModelConfig {
         ocr_base_url: impl Into<String>,
         ocr_model_name: impl Into<String>,
         ocr_cache_dir: impl Into<String>,
-        layout_base_url: impl Into<String>,
-        layout_model_name: impl Into<String>,
-        layout_filename: impl Into<String>,
     ) -> Self {
         Self {
             ocr_base_url: ocr_base_url.into(),
             ocr_model_name: ocr_model_name.into(),
             ocr_cache_dir: ocr_cache_dir.into(),
-            layout_base_url: layout_base_url.into(),
-            layout_model_name: layout_model_name.into(),
-            layout_filename: layout_filename.into(),
         }
     }
 
@@ -62,16 +49,5 @@ impl ModelConfig {
             "vocab.txt",
             "generation_config.json",
         ]
-    }
-
-    pub fn layout_model_file_url(&self) -> String {
-        format!(
-            "{}/{}/resolve/main/{}",
-            self.layout_base_url.trim_end_matches('/'),
-            self.layout_model_name
-                .trim_start_matches('/')
-                .trim_end_matches('/'),
-            self.layout_filename.trim_start_matches('/')
-        )
     }
 }
