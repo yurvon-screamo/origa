2ac272ce811e156c2c8f818db558f5fc0c30f0ad
yurvon-screamo <yurvon@ya.ru>
2026-03-11 08:38:00 +0300

    Commit changes

--- origa_ui/src/pages/sets/content.rs
+++ origa_ui/src/pages/sets/content.rs
@@ -1,7 +1,8 @@
+use super::filters::{LevelFilter, TypeFilter};
 use super::import_set_preview_modal::ImportSetPreviewModal;
 use super::sets_level_group::SetsLevelGroup;
 use super::types::SetInfo;
-use crate::ui_components::{Spinner, Text, TextSize, TypographyVariant};
+use crate::ui_components::{Input, Spinner, Tag, TagVariant, Text, TextSize, TypographyVariant};
 use crate::well_known_set::WellKnownSetLoaderImpl;
 use leptos::prelude::*;
 use leptos::task::spawn_local;
@@ -16,6 +17,9 @@ pub fn SetsContent() -> impl IntoView {
     let preview_set_id = RwSignal::new(String::new());
     let preview_set_title = RwSignal::new(String::new());
     let current_user = use_context::<RwSignal<Option<User>>>().expect("current_user context");
+    let level_filter = RwSignal::new(LevelFilter::default());
+    let type_filter = RwSignal::new(TypeFilter::default());
+    let search = RwSignal::new(String::new());
 
     let loader = WellKnownSetLoaderImpl::new();
     let sets_for_load = sets;
@@ -60,6 +64,24 @@ pub fn SetsContent() -> impl IntoView {
         });
     });
 
+    let filtered_sets = Memo::new(move |_| {
+        let level = level_filter.get();
+        let type_f = type_filter.get();
+        let query = search.get().to_lowercase();
+
+        sets.get()
+            .into_iter()
+            .filter(|s| {
+                let matches_level = level.matches(s.level);
+                let matches_type = type_f.matches(s.set_type);
+                let matches_search = query.is_empty()
+                    || s.title.to_lowercase().contains(&query)
+                    || s.description.to_lowercase().contains(&query);
+                matches_level && matches_type && matches_search
+            })
+            .collect::<Vec<_>>()
+    });
+
     let on_import = Callback::new(move |(set_id, title): (String, String)| {
         preview_set_id.set(set_id);
         preview_set_title.set(title);
@@ -77,11 +99,214 @@ pub fn SetsContent() -> impl IntoView {
                 </div>
             </Show>
             <Show when=move || !is_loading.get()>
-                <SetsLevelGroup level=JapaneseLevel::N5 sets=sets on_import=on_import />
-                <SetsLevelGroup level=JapaneseLevel::N4 sets=sets on_import=on_import />
-                <SetsLevelGroup level=JapaneseLevel::N3 sets=sets on_import=on_import />
-                <SetsLevelGroup level=JapaneseLevel::N2 sets=sets on_import=on_import />
-                <SetsLevelGroup level=JapaneseLevel::N1 sets=sets on_import=on_import />
+                <div class="space-y-4 mb-6">
+                    <Input
+                        value=search
+                        placeholder=Signal::derive(|| "Поиск наборов...".to_string())
+                    />
+
+                    <div class="flex flex-wrap gap-2">
+                        <Tag
+                            variant=Signal::derive(move || {
+                                if level_filter.get() == LevelFilter::All {
+                                    TagVariant::Filled
+                                } else {
+                                    TagVariant::Default
+                                }
+                            })
+                            class=Signal::derive(|| "cursor-pointer".to_string())
+                            on_click=Callback::new(move |_: leptos::ev::MouseEvent| {
+                                level_filter.set(LevelFilter::All);
+                            })
+                        >
+                            {LevelFilter::All.label()}
+                        </Tag>
+                        <Tag
+                            variant=Signal::derive(move || {
+                                if level_filter.get() == LevelFilter::N5 {
+                                    TagVariant::Filled
+                                } else {
+                                    TagVariant::Default
+                                }
+                            })
+                            class=Signal::derive(|| "cursor-pointer".to_string())
+                            on_click=Callback::new(move |_: leptos::ev::MouseEvent| {
+                                level_filter.set(LevelFilter::N5);
+                            })
+                        >
+                            {LevelFilter::N5.label()}
+                        </Tag>
+                        <Tag
+                            variant=Signal::derive(move || {
+                                if level_filter.get() == LevelFilter::N4 {
+                                    TagVariant::Filled
+                                } else {
+                                    TagVariant::Default
+                                }
+                            })
+                            class=Signal::derive(|| "cursor-pointer".to_string())
+                            on_click=Callback::new(move |_: leptos::ev::MouseEvent| {
+                                level_filter.set(LevelFilter::N4);
+                            })
+                        >
+                            {LevelFilter::N4.label()}
+                        </Tag>
+                        <Tag
+                            variant=Signal::derive(move || {
+                                if level_filter.get() == LevelFilter::N3 {
+                                    TagVariant::Filled
+                                } else {
+                                    TagVariant::Default
+                                }
+                            })
+                            class=Signal::derive(|| "cursor-pointer".to_string())
+                            on_click=Callback::new(move |_: leptos::ev::MouseEvent| {
+                                level_filter.set(LevelFilter::N3);
+                            })
+                        >
+                            {LevelFilter::N3.label()}
+                        </Tag>
+                        <Tag
+                            variant=Signal::derive(move || {
+                                if level_filter.get() == LevelFilter::N2 {
+                                    TagVariant::Filled
+                                } else {
+                                    TagVariant::Default
+                                }
+                            })
+                            class=Signal::derive(|| "cursor-pointer".to_string())
+                            on_click=Callback::new(move |_: leptos::ev::MouseEvent| {
+                                level_filter.set(LevelFilter::N2);
+                            })
+                        >
+                            {LevelFilter::N2.label()}
+                        </Tag>
+                        <Tag
+                            variant=Signal::derive(move || {
+                                if level_filter.get() == LevelFilter::N1 {
+                                    TagVariant::Filled
+                                } else {
+                                    TagVariant::Default
+                                }
+                            })
+                            class=Signal::derive(|| "cursor-pointer".to_string())
+                            on_click=Callback::new(move |_: leptos::ev::MouseEvent| {
+                                level_filter.set(LevelFilter::N1);
+                            })
+                        >
+                            {LevelFilter::N1.label()}
+                        </Tag>
+                    </div>
+
+                    <div class="flex flex-wrap gap-2">
+                        <Tag
+                            variant=Signal::derive(move || {
+                                if type_filter.get() == TypeFilter::All {
+                                    TagVariant::Filled
+                                } else {
+                                    TagVariant::Default
+                                }
+                            })
+                            class=Signal::derive(|| "cursor-pointer".to_string())
+                            on_click=Callback::new(move |_: leptos::ev::MouseEvent| {
+                                type_filter.set(TypeFilter::All);
+                            })
+                        >
+                            {TypeFilter::All.label()}
+                        </Tag>
+                        <Tag
+                            variant=Signal::derive(move || {
+                                if type_filter.get() == TypeFilter::Jlpt {
+                                    TagVariant::Filled
+                                } else {
+                                    TagVariant::Default
+                                }
+                            })
+                            class=Signal::derive(|| "cursor-pointer".to_string())
+                            on_click=Callback::new(move |_: leptos::ev::MouseEvent| {
+                                type_filter.set(TypeFilter::Jlpt);
+                            })
+                        >
+                            {TypeFilter::Jlpt.label()}
+                        </Tag>
+                        <Tag
+                            variant=Signal::derive(move || {
+                                if type_filter.get() == TypeFilter::Migii {
+                                    TagVariant::Filled
+                                } else {
+                                    TagVariant::Default
+                                }
+                            })
+                            class=Signal::derive(|| "cursor-pointer".to_string())
+                            on_click=Callback::new(move |_: leptos::ev::MouseEvent| {
+                                type_filter.set(TypeFilter::Migii);
+                            })
+                        >
+                            {TypeFilter::Migii.label()}
+                        </Tag>
+                        <Tag
+                            variant=Signal::derive(move || {
+                                if type_filter.get() == TypeFilter::SpyFamily {
+                                    TagVariant::Filled
+                                } else {
+                                    TagVariant::Default
+                                }
+                            })
+                            class=Signal::derive(|| "cursor-pointer".to_string())
+                            on_click=Callback::new(move |_: leptos::ev::MouseEvent| {
+                                type_filter.set(TypeFilter::SpyFamily);
+                            })
+                        >
+                            {TypeFilter::SpyFamily.label()}
+                        </Tag>
+                        <Tag
+                            variant=Signal::derive(move || {
+                                if type_filter.get() == TypeFilter::Duolingo {
+                                    TagVariant::Filled
+                                } else {
+                                    TagVariant::Default
+                                }
+                            })
+                            class=Signal::derive(|| "cursor-pointer".to_string())
+                            on_click=Callback::new(move |_: leptos::ev::MouseEvent| {
+                                type_filter.set(TypeFilter::Duolingo);
+                            })
+                        >
+                            {TypeFilter::Duolingo.label()}
+                        </Tag>
+                    </div>
+                </div>
+
+                <SetsLevelGroup
+                    level=JapaneseLevel::N5
+                    sets=filtered_sets
+                    type_filter=type_filter
+                    on_import=on_import
+                />
+                <SetsLevelGroup
+                    level=JapaneseLevel::N4
+                    sets=filtered_sets
+                    type_filter=type_filter
+                    on_import=on_import
+                />
+                <SetsLevelGroup
+                    level=JapaneseLevel::N3
+                    sets=filtered_sets
+                    type_filter=type_filter
+                    on_import=on_import
+                />
+                <SetsLevelGroup
+                    level=JapaneseLevel::N2
+                    sets=filtered_sets
+                    type_filter=type_filter
+                    on_import=on_import
+                />
+                <SetsLevelGroup
+                    level=JapaneseLevel::N1
+                    sets=filtered_sets
+                    type_filter=type_filter
+                    on_import=on_import
+                />
             </Show>
             <ImportSetPreviewModal
                 is_open=preview_modal_open
