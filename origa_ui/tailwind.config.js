e587d5ccd5d317d6931cbc7f62a41dbaa058ed5d
yurvon-screamo <yurvon@ya.ru>
2026-03-14 08:35:06 +0300

    qlty: f,t

--- origa_ui/tailwind.config.js
+++ origa_ui/tailwind.config.js
@@ -4,17 +4,19 @@ module.exports = {
 	theme: {
 		extend: {
 			padding: {
-				'safe-t': 'env(safe-area-inset-top, 0px)',
-				'safe-b': 'env(safe-area-inset-bottom, 0px)',
-				'safe-l': 'env(safe-area-inset-left, 0px)',
-				'safe-r': 'env(safe-area-inset-right, 0px)',
-				'safe-x': 'env(safe-area-inset-left, 0px) env(safe-area-inset-right, 0px)',
-				'safe-y': 'env(safe-area-inset-top, 0px) env(safe-area-inset-bottom, 0px)',
-				'safe': 'env(safe-area-inset-top, 0px) env(safe-area-inset-right, 0px) env(safe-area-inset-bottom, 0px) env(safe-area-inset-left, 0px)',
+				"safe-t": "env(safe-area-inset-top, 0px)",
+				"safe-b": "env(safe-area-inset-bottom, 0px)",
+				"safe-l": "env(safe-area-inset-left, 0px)",
+				"safe-r": "env(safe-area-inset-right, 0px)",
+				"safe-x":
+					"env(safe-area-inset-left, 0px) env(safe-area-inset-right, 0px)",
+				"safe-y":
+					"env(safe-area-inset-top, 0px) env(safe-area-inset-bottom, 0px)",
+				safe: "env(safe-area-inset-top, 0px) env(safe-area-inset-right, 0px) env(safe-area-inset-bottom, 0px) env(safe-area-inset-left, 0px)",
 			},
 			margin: {
-				'safe-t': 'env(safe-area-inset-top, 0px)',
-				'safe-b': 'env(safe-area-inset-bottom, 0px)',
+				"safe-t": "env(safe-area-inset-top, 0px)",
+				"safe-b": "env(safe-area-inset-bottom, 0px)",
 			},
 		},
 	},
