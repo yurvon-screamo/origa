ac42fa557a773cff26eb3197d90ec70fe9a50a4d
yurvon-screamo <yurvon@ya.ru>
2026-04-05 17:27:43 +0300

    fix oidc

--- end2end/playwright.config.ts
+++ end2end/playwright.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     fullyParallel: true,
     forbidOnly: isCI,
     retries: isCI ? 1 : 0,
-    workers: isCI ? 2 : 3,
+    workers: isCI ? 2 : 8,
     reporter: [
         [
             "html",
